7c68698ec75b635e8cb34c6c3f8653d918032074
= <=>
2025-08-24 15:00:40 -0300

    Variáveis e tipos de dados.

---
+++ html-css-js/JavaScript-Completo/aula2-variaveis-e-constantes/script.js
@@ -0,0 +1,22 @@
+/* Diferentes formas de declarar variáveis */
+
+/* 
+
+var nome = "Felipe"; // var são variáveis de escopo global ou de função
+let sobrenome = "Lamas da Silva"; // let são variáveis que podem ter seus valores alterados e são de escopo de bloco
+const CIDADE = "São Paulo"; // Constantes são variáveis que não podem ser alteradas
+
+*/
+
+/* Tipos de dados */
+
+let nome = "Felipe"; // String
+let idade = 31; // Number
+let altura = 1.75; // Number
+let possuiCarro = true; // Boolean
+let hobbies = ["futebol", "leitura", "cinema"]; // Array
+let endereco = {
+    rua: "Av. Paulista",
+    numero: 1578,
+    cidade: "São Paulo"
+}; // Objeto
