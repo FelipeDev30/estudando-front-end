56b1d581254ae7f69f94f30635bf90b533945fc9
= <=>
2025-08-24 15:13:46 -0300

    Aprendendo sobre arrays

---
+++ html-css-js/JavaScript-Completo/aula3-arrays/script.js
@@ -0,0 +1,13 @@
+/* 
+
+O que são arrays?
+
+Um array é uma estrutura de dados que permite armazenar múltiplos valores em uma única variável. Os valores em um array são chamados de elementos e podem ser acessados por meio de seus índices.
+
+*/
+
+let frutas = ["maçã", "banana", "laranja"];
+console.log(frutas[1]);
+
+frutas.push("uva");
+console.log(frutas[3]);
