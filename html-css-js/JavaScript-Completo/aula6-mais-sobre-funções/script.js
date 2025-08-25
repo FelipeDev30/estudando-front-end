e4f3f94f2f67dc1de07d936e37d77a1b04d25868
= <=>
2025-08-24 23:47:02 -0300

    Mais sobre funções (funções anônimais)

---
+++ html-css-js/JavaScript-Completo/aula6-mais-sobre-funções/script.js
@@ -0,0 +1,18 @@
+/* Formas de declarações de funções */
+
+/* Função anônima */
+
+let func = function() {
+    console.log("Hello, World!");
+}
+
+func();
+
+/* Função anônima */
+
+let funcao = function nomeDaFuncao() {
+    console.log("Hello, World!");
+}
+
+funcao();
+
