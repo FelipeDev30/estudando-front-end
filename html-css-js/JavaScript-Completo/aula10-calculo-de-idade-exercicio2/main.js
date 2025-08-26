f3ff594620b14821b9082b51a58033a17cec1d9e
= <=>
2025-08-25 23:19:38 -0300

    Alterando arquivo do ultimo exercicio

--- html-css-js/JavaScript-Completo/aula10-calculo-de-idade-exercicio2/main.js
+++ html-css-js/JavaScript-Completo/aula10-calculo-de-idade-exercicio2/main.js
@@ -1,14 +1,14 @@
 form = document.getElementById('form');
 resultado = document.getElementById('resultado');
 
-form.addEventListener('submit', function(event) {
+form.addEventListener(`submit`, function(event) {
     event.preventDefault();
-    const anoNascimento = Number(document.getElementById('anoNascimento').value);
-    const idade = calcularIdade(anoNascimento);
+    const anoNascimento = Number(document.getElementById(`anoNascimento`).value)
+    const idade = calcularIdade(anoNascimento)
     mostrarResultado(idade);
-});
+})
 
-function calcularIdade(anoNascimento) {
+function calcularIdade(anoNascimento){
     const anoAtual = new Date().getFullYear();
     return anoAtual - anoNascimento;
 }
