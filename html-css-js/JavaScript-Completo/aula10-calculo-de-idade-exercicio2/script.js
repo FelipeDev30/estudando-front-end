71dd89911883c64d02a24416367bdec27a7de455
= <=>
2025-08-25 23:08:12 -0300

    Calculadora de idade dinâmica utilizando conceitos de Document Object Model

---
+++ html-css-js/JavaScript-Completo/aula10-calculo-de-idade-exercicio2/script.js
@@ -0,0 +1,19 @@
+form = document.getElementById('form');
+resultado = document.getElementById('resultado');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const anoNascimento = Number(document.getElementById('anoNascimento').value);
+    const idade = calcularIdade(anoNascimento);
+    mostrarResultado(idade);
+});
+
+function calcularIdade(anoNascimento) {
+    const anoAtual = new Date().getFullYear();
+    return anoAtual - anoNascimento;
+}
+
+function mostrarResultado(idade) {
+    resultado.innerHTML = `Você tem ${idade} anos.`;
+}
+
