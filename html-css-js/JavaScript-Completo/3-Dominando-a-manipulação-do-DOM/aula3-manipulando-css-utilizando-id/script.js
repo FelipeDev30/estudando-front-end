4c56dc67ba5ac75db72ab8a1f019568412e37a0e
= <=>
2025-08-31 13:47:10 -0300

    Manipulando estilos através do ID, utilizando JavaScript

---
+++ html-css-js/JavaScript-Completo/3-Dominando-a-manipulação-do-DOM/aula3-manipulando-css-utilizando-id/script.js
@@ -0,0 +1,8 @@
+let el = document.getElementById("box");
+el.style.backgroundColor = "red";
+el.style.width = "200px";
+el.style.height = "100px";
+el.style.border = "2px solid black";
+el.style.display = "flex";
+el.style.alignItems = "center";
+el.style.justifyContent = "center";
