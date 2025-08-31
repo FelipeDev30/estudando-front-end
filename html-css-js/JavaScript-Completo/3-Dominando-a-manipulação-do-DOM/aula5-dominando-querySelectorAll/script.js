04a8914d1170d8d4cb21f3baff0d4b726e01b07c
= <=>
2025-08-31 14:09:18 -0300

    Utilizando innerHTML para alterar textos

---
+++ html-css-js/JavaScript-Completo/3-Dominando-a-manipulação-do-DOM/aula5-dominando-querySelectorAll/script.js
@@ -0,0 +1,13 @@
+/*
+
+let els = document.querySelectorAll('#box .inside-testes');
+els[0].style.color = "blue";
+els[0].style.fontSize = "20px";
+els[1].style.color = "green";
+els[1].style.fontSize = "18px"; 
+
+*/
+
+let els = document.querySelectorAll('#box .inside-testes');
+els[0].innerHTML = "Novo texto para o primeiro elemento";
+els[1].innerHTML = "Novo texto para o segundo elemento";
