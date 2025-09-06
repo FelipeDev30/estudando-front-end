b8b82eda4ab7c1dc1472de5a28bf881bccaab8f9
= <=>
2025-09-06 13:35:25 -0300

    Nota de estudo sobre destructuring

--- html-css-js/JavaScript-Completo/4-JavaScript-Moderno/aula4-destructure/script.js
+++ html-css-js/JavaScript-Completo/4-JavaScript-Moderno/aula4-destructure/script.js
@@ -1,3 +1,10 @@
+/* 
+
+# O que é Destructuring?
+
+Destructuring é uma expressão do JavaScript que possibilita a extração de dados de arrays ou objetos em variáveis distintas, facilitando o acesso e manipulação desses dados.
+
+*/
 const arr = ['Guilherme', 'Miranda', 30 , 'Rio de Janeiro'];
 
 const [nome, sobrenome, idade, cidade] = arr;
