80c27e0ff647f7554414e11522f2f50fdfe8ebcc
= <=>
2025-09-06 11:25:47 -0300

    Fixando exercicio

--- html-css-js/JavaScript-Completo/3-Dominando-a-manipulação-do-DOM/aula8-criando-um-slider-exercicio/script.js
+++ html-css-js/JavaScript-Completo/3-Dominando-a-manipulação-do-DOM/aula8-criando-um-slider-exercicio/script.js
@@ -1,24 +1,20 @@
-            var lastIndex = 0;
+var lastIndex = 0;
+var images = document.querySelectorAll('.container img');
 
-            var images = document.querySelectorAll('.container img');
+images.forEach((item, index) => {
+    document.querySelectorAll('.bullet-single')[index].addEventListener('click', ()=>{
+        let lastImage = document.querySelectorAll('.container img')[lastIndex];
+        let actualImage = document.querySelectorAll('.container img')[index];
 
-            images.forEach((item,index)=>{
-                document.querySelectorAll('.bullet-single')[index].addEventListener('click',()=>{
-                    let lastImage = document.querySelectorAll('.container img')[lastIndex];
-                    let actualImage = document.querySelectorAll('.container img')[index];
+        //Troca de imagens
+        lastImage.style.opacity = 0;
+        actualImage.style.opacity = 1;
 
-                    //Resetar as bullets e setar a nova com base na imagem.
+        //Reset das bullets
+        document.querySelectorAll('.bullet-single')[lastIndex].classList.remove('active-bullet');
+        document.querySelectorAll('.bullet-single')[index].classList.add('active-bullet');
 
-                    document.querySelectorAll('.bullet-single')[lastIndex]
-                    .classList.remove('active-bullet');
-                    
-                    document.querySelectorAll('.bullet-single')[index]
-                    .classList.add('active-bullet');
+        lastIndex = index;
 
-                    lastImage.style.opacity = 0;
-                    actualImage.style.opacity = 1;
-
-                    lastIndex = index;
-                    
-                })
-            })
+    })
+});
