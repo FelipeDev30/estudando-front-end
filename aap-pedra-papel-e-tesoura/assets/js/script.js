947eb35d3e4f263af758490d8eccce0b5147cc6a
= <=>
2025-09-12 02:26:28 -0300

    Criando o jogador bot

--- aap-pedra-papel-e-tesoura/assets/js/script.js
+++ aap-pedra-papel-e-tesoura/assets/js/script.js
@@ -1,19 +1,38 @@
-/* Selecionar opções do jogador */
+const elementos = document.querySelectorAll('.player-options div > img');
+let playerOpt = "";
+let enemyOpt = "";
 
-var elementos = document.querySelectorAll('.player-options div > img');
-var playerOpt = "";
+// Declarar enemyOptions no escopo global
+const enemyOptions = document.querySelectorAll('.enemy-options div > img');
+
+function resetEnemy() {
+    enemyOptions.forEach(img => {
+        img.style.opacity = 0.3;
+    });
+}
+
+function inimigoJogar() {
+    const rand = Math.floor(Math.random() * enemyOptions.length);
+    resetEnemy();
+
+    enemyOptions[rand].style.opacity = 1;
+    enemyOpt = enemyOptions[rand].getAttribute('opt');
+
+    alert(`Player: ${playerOpt} | Inimigo: ${enemyOpt}`);
+}
+
 function resetOpacityPlayer() {
-    for(var i = 0; i < elementos.length; i++){
-        elementos[i].style.opacity = 0.3;
-    } 
+    elementos.forEach(img => {
+        img.style.opacity = 0.3;
+    });
 }
 
-for(var i = 0; i < elementos.length; i++) {
-    elementos[i].addEventListener('click', (t) =>{
+elementos.forEach(img => {
+    img.addEventListener('click', (e) => {
         resetOpacityPlayer();
-        t.target.style.opacity = 1;
-        playerOpt = t.target.getAttribute('opt');
-    })
-}
+        e.target.style.opacity = 1;
+        playerOpt = e.target.getAttribute('opt');
 
-/* Criando jogador bot */
+        inimigoJogar();
+    });
+});
